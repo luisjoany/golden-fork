38aede8ad761134b9881c8bf7d20c47744a40dff
Fabian Correa <becasfabian@hotmail.com>
2021-11-24 23:16:35 -0500

    Frontend crud

--- frontend/src/app/components/ordenes/ordenes.component.ts
+++ frontend/src/app/components/ordenes/ordenes.component.ts
@@ -1,21 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Orden } from 'src/app/models/orden';
 import { OrdenService } from '../../services/orden.service';
 
 @Component({
   selector: 'app-ordenes',
   templateUrl: './ordenes.component.html',
   styleUrls: ['./ordenes.component.css'],
-  providers: [OrdenService]
+  providers:[OrdenService],
 })
 export class OrdenesComponent implements OnInit {
 
-  constructor(private ordenService: OrdenService) { }
+  constructor(public ordenService: OrdenService) { }
 
   ngOnInit(): void {
+    this.getOrdenes();
   }
 
-  addOrden(){
-
+  addOrden(form: NgForm){
+    if (form.value._id) {
+      this.ordenService.putOrdenes(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.getOrdenes();
+      });
+    } else {
+      this.ordenService.putOrdenes(form.value).subscribe((res) => {
+        this.getOrdenes();
+        this.resetForm(form);
+      });
+    }
   }
+
+  getOrdenes() {
+    this.ordenService.getOrdenes().subscribe((res) => {
+      this.ordenService.ordenes = res;
+    });
+  }
+
+  editOrdenes(orden: Orden) {
+    this.ordenService.selectedOrden = orden;
+  }
+
+  deleteOrdenes(_id: string, form: NgForm) {
+    if (confirm("Are you sure you want to delete it?")) {
+      this.ordenService.deleteOrdenes(_id).subscribe((res) => {
+        this.getOrdenes();
+        this.resetForm(form);
+      });
+    }
+  }
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.ordenService.selectedOrden = new Orden();
+    }
+  }
+
 
 }
--- frontend/src/app/services/orden.service.ts
+++ frontend/src/app/services/orden.service.ts
@@ -9,23 +9,24 @@ import { Orden } from '../models/orden';
 export class OrdenService {
 
   selectedOrden: Orden;
-  ordenes!: Orden[];
+  ordenes: Orden[] = [];
+  readonly URL_API = 'http://localhost:3000/api/orden'
 
-  readonly URL_API = 'http://locahost:3000/api/orden'
   constructor(private http: HttpClient) {
     this.selectedOrden = new Orden();
   }
 
-  getOrdenes(){
-    return this.http.get(this.URL_API);
+
+  postOrdenes(orden: Orden){
+    return this.http.post(this.URL_API, orden);
   }
 
-  posOrdenes(orden: Orden){
-    return this.http.post(this.URL_API, orden)
+  getOrdenes(){
+    return this.http.get<Orden[]>(this.URL_API);
   }
 
   putOrdenes(orden: Orden){
-    return this.http.put(this.URL_API + `${orden._id}`, orden);
+    return this.http.put(this.URL_API + `/${orden._id}`, orden);
   }
 
   deleteOrdenes(_id: string){
